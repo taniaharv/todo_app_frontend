2b19488928bf854c9bc46155ed6060d3773aa8e6
Tania Harvey <taniaharvey@hotmail.com>
2019-10-16 12:11:20 +0100

    first components added

---
+++ src/components/Intro.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Intro extends React.Component {
+
+    render() {
+        return (
+            <p>Welcome to your best day ever!</p>
+        );
+    }
+
+
+}
+
+export default Intro;
